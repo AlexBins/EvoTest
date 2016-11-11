9368df2e691ce17cc047f30134ad3726e8cdf694
AlexBins <abinsmaier@gmail.com>
2016-11-11 15:04:51 +0100

    Minor change to testscript

--- src/test_controlmatrix.m
+++ src/test_controlmatrix.m
@@ -5,6 +5,5 @@ ctrl2 = pl.CalculateControlSignal(2, 1.125);
 ctrl1 = pc.CalculateControlSignal(2, 1.125);
 mat = [ctrl1; ctrl1; ctrl2; ctrl1; ctrl2; ctrl1;];
 
-sc.DisplayScenario(0);
 sc.ExecuteControlMatrix(mat);
 sc.Replay(1 / 60, 1);
