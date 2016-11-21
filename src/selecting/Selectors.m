04c2d94d653bdce195b0104e2e011e8d3447ba0e
AlexBins <abinsmaier@gmail.com>
2016-11-21 16:37:08 +0100

    Addded Selecting functionality

---
+++ src/selecting/Selectors.m
@@ -0,0 +1,43 @@
+classdef Selectors
+    methods (Static)
+        function selector = get_fitnessWeighted()
+            function selected = select(chromosomes)
+                % p(x = x1) = (1 / n) * prop(x1)
+                % n = length(chromosomes)
+                % prop(x1) = if directpropotionalfitnessfunction
+                %   Fitness(x1) / avg
+                %       if indirectproportionalfitnessfunction
+                %   avg / Fitness(x1)
+                % not proven that sum over all xi in chromosomes of
+                % p(x=xi) = 1
+                % indirect proportional does not work this way.
+                % TODO: fix indirect Proporitonality
+                n = length(chromosomes);
+                total_fitness = 0;
+                for i = 1:n
+                    total_fitness = total_fitness + chromosomes(i).Fitness;
+                end
+                current_fitness = 0;
+                idx = 0;
+                rand_number = randi(total_fitness);
+                while current_fitness < rand_number
+                    idx = idx + 1;
+                    current_fitness =...
+                        current_fitness + chromosomes(idx).Fitness;
+                end
+                selected = chromosomes(idx);
+            end
+            selector = @select;
+        end
+        
+        function selector = get_uniform()
+            function selected = select(chromosomes)
+                n = length(chromosomes);
+                selected = chromosomes(randi(n));
+            end
+            selector = @select;
+        end
+    end
+    
+end
+
