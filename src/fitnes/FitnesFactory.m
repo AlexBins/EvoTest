5b313d375a1b667b9f849ab7d06050e1bb78bb82
= <manuel.mende@gmx.de>
2016-11-22 11:17:01 +0100

    added primitive fitnes function

---
+++ src/fitnes/FitnesFactory.m
@@ -0,0 +1,23 @@
+classdef FitnesFactory
+    properties
+    end
+    
+    methods(Static)
+        function fitnes_func = get_simple(good_fitnes_limit)
+            function fitnes = fit(chr)
+                scenario = StaticScenario(chr.carx, chr.cary, chr.carangle, chr.slotlength, chr.slotdepth);
+                [min_distance collision] = scenario.RunParkingPilot();
+                if collision
+                    penalty = 0;
+                else
+                    penalty = 1;
+                end
+                fitnes = 1/(penalty*min_distance+(1/good_fitnes_limit));
+                scenario.Replay(1/60, 2);
+            end
+            fitnes_func = @fit;
+        end
+    end
+    
+end
+
