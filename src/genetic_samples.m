75886ee36281b903f0ffdaeb7b40f082b7362d7e
AlexBins <abinsmaier@gmail.com>
2016-11-27 21:54:19 +0100

    Extended and reorganized Documentation in semantical order Changed GA test script to use correct selection function

--- src/genetic_samples.m
+++ src/genetic_samples.m
@@ -8,13 +8,19 @@ rational = MutatorFactory.get_range(0.1);
 fit = FitnesFactory.get_desired_mindistance(50, 0.05);
 
 % a selector
-select =  SelectCandidateFactory.get_generic(Selectors.get_uniform());
+select =  SelectCandidateFactory.get_generic(Selectors.get_fitnessWeighted());
 
 % a merger
 merger = MergeFactory.get_generic(Merges.get_naiverandommerge());
 
 gen = GeneticAlgorithm(10, 1, 0.5, fit, select, merger, rational);
 gen.main(50);
-sc = gen.Population(1).get_scenario();
-sc.RunParkingPilot();
-sc.Replay(0.04, 3);
+
+for i = 1:length(gen.Population)
+    close all;
+    sc = gen.Population(i).get_scenario();
+    sc.RunParkingPilot();
+    sc.Replay(0.04, 3);
+    
+    pause(2);
+end
