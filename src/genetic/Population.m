ef69ebc4aef63f2a58af60a65fa95186d53b5dc0
Manuel Mende <manuel.mende@gmx.de>
2017-01-04 15:29:47 +0100

    Added extend to population

--- src/genetic/Population.m
+++ src/genetic/Population.m
@@ -19,12 +19,17 @@ classdef Population < handle
             current_idx = length(self.chromosomes);
             self.chromosomes(current_idx+1) = chromosome;
         end
+        
+        function extend(self, chromosomes)
+            current_idx = length(self.chromosomes);
+            self.chromosomes(current_idx+1:current_idx+length(chromosomes)) = chromosomes;
+        end
 
         function reduce(self)
-            current_worst_value = 1; 
-            current_worst_idx = 1;
             overflow = length(self.chromosomes) - self.size;
             for i = 1:overflow
+                current_worst_value = 1; 
+                current_worst_idx = 1;
                 for j = 1:length(self.chromosomes)
                     current_fitness = self.chromosomes(j).fitness;
                     if (current_fitness < current_worst_value)
