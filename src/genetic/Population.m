35ac14a478c8235f0f485f36d8faaf0ae7e4a1f0
Manuel Mende <manuel.mende@gmx.de>
2017-01-04 13:48:08 +0100

    Implementation Population Datastructure

--- src/genetic/Population.m
+++ src/genetic/Population.m
@@ -1,10 +1,40 @@
-classdef Population
+classdef Population < handle
     
     properties
-        
+        chromosomes;
+        size;
     end
     
     methods
+        function population = Population(size)
+            population.size = size;
+            population.chromosomes = Chromosome.get_random();
+            for i = 2:size
+                chr = Chromosome.get_random();
+                population.add(chr);
+            end
+        end
+        
+        function add(self, chromosome)
+            current_idx = length(self.chromosomes);
+            self.chromosomes(current_idx+1) = chromosome;
+        end
+
+        function reduce(self)
+            current_worst_value = 1; 
+            current_worst_idx = 1;
+            overflow = length(self.chromosomes) - self.size;
+            for i = 1:overflow
+                for j = 1:length(self.chromosomes)
+                    current_fitness = self.chromosomes(j).fitness;
+                    if (current_fitness < current_worst_value)
+                        current_worst_idx = j;
+                        current_worst_value = current_fitness;
+                    end
+                end
+                self.chromosomes(current_worst_idx) = [];
+            end
+        end
     end
     
 end
