4c64e62ec345bbf54b7679a2523cfde56e43e551
Manuel Mende <manuel.mende@gmx.de>
2017-01-16 15:12:33 +0100

    added some evaluation

--- src/meineSkripte/eval_multi.m
+++ src/meineSkripte/eval_multi.m
@@ -1,9 +1,9 @@
-function eval_multi(pop_instanz)
+function eval_multi(ga_instanz)
     hold on;
-    pops = pop_instanz.pops;
-    colors = ['r-', 'b-', 'g-', 'y-', 'c-', 'k-', 'm-'];
+    pops = ga_instanz.pops;
+    colors = ['r-'; 'b-'; 'g-'; 'y-'; 'c-'; 'k-'; 'm-'];
     for i = 1:length(pops)
-        fprintf('\n\nPloting a new Population:\n');
+        fprintf('\n\nPloting a new Population (%s) \n', colors(i));
         tcs = extract_tcs(pops(i).chromosomes);
         plot_tcs(tcs, colors(mod(i,7)));
         fprintf('Collecting information:\n');
@@ -12,12 +12,12 @@ function eval_multi(pop_instanz)
 end
 
 function plot_tcs(testcases, color)
-    for i=1:length(testcases)
+    for i=1:size(testcases, 2)
         tc = testcases(:,i);
         x = [tc(1) tc(1)+cos(tc(3))];
         y = [tc(2) tc(2)+sin(tc(3))];
         plot(x,y, color);
         plot(tc(1), tc(2), 'b*');
-        fprintf('Testcase: [%f, %f, %f]\n', tc(1), tc(2), tc(3));
+        fprintf('Testcase: [%f, %f, %f] -> %s\n', tc(1), tc(2), tc(3), color);
     end
 end
--- src/meineSkripte/visualize_tcs.m
+++ src/meineSkripte/visualize_tcs.m
@@ -2,7 +2,7 @@ function visualize_tcs(testcases)
     hold on;
     avg_fitnes = mean(testcases(6,:));
     fprintf('There are %d testcases\n', length(testcases));
-    for i=1:length(testcases)
+    for i=1:size(testcases, 2)
         tc = testcases(:,i);
         x = [tc(1) tc(1)+cos(tc(3))];
         y = [tc(2) tc(2)+sin(tc(3))];
